0672304f31bc5346c55d0a284e3387dc55621b3e
Vova Melnyk <hagmen4339@gmail.com>
2022-05-30 20:44:37 +0300

    add tasks

--- index.js
+++ index.js
@@ -39,11 +39,22 @@
 
 // ```js
 // const user = {
-//   name: 'Mango',
+//   name: "Mango",
 //   age: 20,
-//   hobby: 'html',
+//   hobby: "html",
 //   premium: true,
 // };
+
+// user.mood = "happy";
+// user.hobby = "skydiving";
+// user.premium = false;
+
+// const keys = Object.keys(user);
+// for (const key of keys) {
+//   console.log(`${key}:${user[key]}`);
+// }
+
+// console.log("keys", keys);
 // ```
 
 // ## Example 2 - метод Object.values()
@@ -60,6 +71,34 @@
 //   Ann: 160,
 //   Pete: 130,
 // };
+
+// console.log(salaries);
+// const salaries1 = {};
+// // const salaries = {};
+// // 1) Робимо ф - ю де параметром є обєкт
+// // 2) Потрібно просумувати значення обєкта
+// // 3) Додати перевірка на порожній обєкт
+
+// const calcSalaries = function (salaries) {
+//   const values = Object.values(salaries);
+//   if (values.length === 0) {
+//     return 0;
+//   }
+//   let total = 0;
+//   for (const value of values) {
+//     total += value;
+//   }
+//   return total;
+// };
+// // calcSalaries(salaries1);
+// console.log(calcSalaries(salaries));
+// let total = 0;
+// for (const salary in salaries) {
+//   total += salaries[salary];
+// }
+
+// console.log(total);
+
 // ```
 
 // ## Example 3 - Массив объектов
@@ -72,11 +111,24 @@
 
 // ```js
 // const stones = [
-//   { name: 'Изумруд', price: 1300, quantity: 4 },
-//   { name: 'Бриллиант', price: 2700, quantity: 3 },
-//   { name: 'Сапфир', price: 400, quantity: 7 },
-//   { name: 'Щебень', price: 200, quantity: 2 },
+//   { name: "Изумруд", price: 1300, quantity: 4 },
+//   { name: "Бриллиант", price: 2700, quantity: 3 },
+//   { name: "Сапфир", price: 400, quantity: 7 },
+//   { name: "Щебень", price: 200, quantity: 2 },
 // ];
+
+// function calcTotalPrice(stones, stoneName) {
+//   let targetStone = null;
+//   for (const stone of stones) {
+//     if (stone.name === stoneName) {
+//       targetStone = stone;
+//     }
+//   }
+//   return targetStone.price * targetStone.quantity;
+// }
+
+// console.log(calcTotalPrice(stones, "Щебень"));
+
 // ```
 
 // ## Example 4 - Комплексные задачи
@@ -90,61 +142,61 @@
 //  * Типов транзацкий всего два.
 //  * Можно положить либо снять деньги со счета.
 //  */
-// const Transaction = {
-//   DEPOSIT: 'deposit',
-//   WITHDRAW: 'withdraw',
-// };
+const Transaction = {
+  DEPOSIT: 'deposit',
+  WITHDRAW: 'withdraw',
+};
 
 // /*
 //  * Каждая транзакция это объект со свойствами: id, type и amount
 //  */
 
-// const account = {
-//   // Текущий баланс счета
-//   balance: 0,
+const account = {
+  // Текущий баланс счета
+  balance: 0,
 
-//   // История транзакций
-//   transactions: [],
+  // История транзакций
+  transactions: [],
 
-//   /*
-//    * Метод создает и возвращает объект транзакции.
-//    * Принимает сумму и тип транзакции.
-//    */
-//   createTransaction(amount, type) {},
+  /*
+   * Метод создает и возвращает объект транзакции.
+   * Принимает сумму и тип транзакции.
+   */
+  createTransaction(amount, type) {},
 
-//   /*
-//    * Метод отвечающий за добавление суммы к балансу.
-//    * Принимает сумму танзакции.
-//    * Вызывает createTransaction для создания объекта транзакции
-//    * после чего добавляет его в историю транзакций
-//    */
-//   deposit(amount) {},
+  /*
+   * Метод отвечающий за добавление суммы к балансу.
+   * Принимает сумму танзакции.
+   * Вызывает createTransaction для создания объекта транзакции
+   * после чего добавляет его в историю транзакций
+   */
+  deposit(amount) {},
 
-//   /*
-//    * Метод отвечающий за снятие суммы с баланса.
-//    * Принимает сумму танзакции.
-//    * Вызывает createTransaction для создания объекта транзакции
-//    * после чего добавляет его в историю транзакций.
-//    *
-//    * Если amount больше чем текущий баланс, выводи сообщение
-//    * о том, что снятие такой суммы не возможно, недостаточно средств.
-//    */
-//   withdraw(amount) {},
+  /*
+   * Метод отвечающий за снятие суммы с баланса.
+   * Принимает сумму танзакции.
+   * Вызывает createTransaction для создания объекта транзакции
+   * после чего добавляет его в историю транзакций.
+   *
+   * Если amount больше чем текущий баланс, выводи сообщение
+   * о том, что снятие такой суммы не возможно, недостаточно средств.
+   */
+  withdraw(amount) {},
 
-//   /*
-//    * Метод возвращает текущий баланс
-//    */
-//   getBalance() {},
+  /*
+   * Метод возвращает текущий баланс
+   */
+  getBalance() {},
 
-//   /*
-//    * Метод ищет и возвращает объект транзации по id
-//    */
-//   getTransactionDetails(id) {},
+  /*
+   * Метод ищет и возвращает объект транзации по id
+   */
+  getTransactionDetails(id) {},
 
-//   /*
-//    * Метод возвращает количество средств
-//    * определенного типа транзакции из всей истории транзакций
-//    */
-//   getTransactionTotal(type) {},
-// };
+  /*
+   * Метод возвращает количество средств
+   * определенного типа транзакции из всей истории транзакций
+   */
+  getTransactionTotal(type) {},
+};
 // ```
